a4a0212ce6261f9e18f8e12e4ffe6a4cb89270ea
Ted Pudlik <tpudlik@gmail.com>
2016-01-12 16:25:36 -0500

    Improved accuracy plot design.

--- reference/make_accuracy_plots.py
+++ reference/make_accuracy_plots.py
@@ -34,29 +34,41 @@ def accuracy_plot(a, b, z, ref, func):
     Matplotlib Figure object.
 
     """
-    imdata = np.ma.masked_invalid(relative_error(ref, func(a, b, z)))
+    imdata = np.ma.masked_invalid(correct_digits(ref, func(a, b, z)))
 
-    cmap = plt.cm.RdYlBu
-    cmap.set_bad('k', 1)
+    cmap = plt.cm.Greens
+    cmap.set_bad('r', 1)
 
     fig, ax = plt.subplots()
-    im = ax.pcolormesh(a, b, imdata, cmap=cmap, vmin=-10, vmax=10)
+    im = ax.pcolormesh(a, b, imdata, cmap=cmap, vmin=0, vmax=17)
     plt.colorbar(im)
 
     ax.set_xlim((np.min(a), np.max(a)))
     ax.set_ylim((np.min(b), np.max(b)))
-
     ax.set_xlabel("a")
     ax.set_ylabel("b")
 
-    ax.set_title("Relative error for z = {:.2e}".format(np.float64(z)))
+    ax.set_title("Correct digits for z = {:.2e}".format(np.float64(z)))
 
     return fig
 
-def relative_error(true, estimate):
-    out = (estimate - true)/np.abs(true)
-    out[true == estimate] = 0  # For np.inf
-    return out
+@np.vectorize
+def correct_digits(true, estimate):
+    if np.isfinite(true) and not np.isfinite(estimate):
+        return 0
+    if not np.isfinite(true) and np.isfinite(estimate):
+        return 0
+    if true == estimate or np.isnan(true) and np.isnan(estimate):
+        # both +-inf or nan
+        return 17
+    if true == 0:
+        return 0
+
+    relative_error = np.abs((estimate - true)/true)
+    if not np.isfinite(relative_error) or relative_error > 1:
+        return 0
+
+    return -np.log10(relative_error)
 
 def make_plot(ref_file, func, fig_fname):
     """Load reference data from ref_file and save an accuracy_plot of func 
@@ -77,16 +89,14 @@ def make_plot(ref_file, func, fig_fname):
 def get_function(name):
     """Return the function to use in computing estimates of hyp1f1."""
 
-    unvectorized = ("new_hyp1f1", "asymptotic_series", "taylor_series",
-                    "taylor_series_frac", "single_fraction", "bessel_series",
-                    "asymptotic_series_muller")
+    vectorized = ("hyp1f1",)
 
     try:
         func = getattr(scipy.special, args.func)
     except:
         func = getattr(hypergeometric, args.func)
 
-    if args.func in unvectorized:
+    if args.func not in vectorized:
         # Eventually new_hyp1f1 will be vectorized, too---but not yet.
         @np.vectorize
         def f(a, b, z):
@@ -112,7 +122,8 @@ if __name__ == '__main__':
                         choices=["hyp1f1", "new_hyp1f1", "taylor_series",
                                  "taylor_series_frac", "single_fraction",
                                  "bessel_series", "asymptotic_series",
-                                 "asymptotic_series_muller"])
+                                 "asymptotic_series_muller",
+                                 "taylor_series_recur"])
     args = parser.parse_args()
     func = get_function(args.func)
 
